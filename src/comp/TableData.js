88a67e3dc2c7c773a588e110cc5708a3a092f57e
Vkym03 <95181508+Vkym03@users.noreply.github.com>
2022-02-07 11:43:31 +0530

    project s

---
+++ src/comp/TableData.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table'
+
+function TableData(props) {
+
+    const editData=()=>{
+        props.history.push("/")
+    }
+
+  const deleteData=()=>{
+alert("Data will Deleted")
+
+    }
+
+  return <div className='col-lg-12 p-5  text-center'>
+  <Table  bordered  className='bg-dark p-5 text-white mt-5'>
+  <thead>
+    <tr>
+      <th><b>Name</b></th>
+      <th><b>Age</b></th>
+      <th><b>Job</b></th>
+      <th><b>Salary</b></th>
+      <th><b>Edit</b></th>
+      <th><b>Delete</b></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td><button className='btn btn-warning' onClick={editData}>Edit</button></td>
+      <td><button className='btn btn-info' onClick={ deleteData}>Delete</button></td>
+    </tr>
+    
+   {/* {
+        map
+    }*/}
+  </tbody>
+</Table>
+
+  </div>
+
+}
+
+export default TableData;

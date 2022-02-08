022ac97a84ef3c45a576aa0593f6a03697e0a8ed
HajiraMarwah <hajiranadia12@gmail.com>
2022-02-08 12:54:03 +0530

    add details change

--- src/comp/AddDetails.js
+++ src/comp/AddDetails.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-// import TableData from './TableData';
+import React,{ useContext, useEffect, useState } from 'react';
+import ContextData from '../context/context';
 
-function AddDetails(props) {
-    const [employeeData, setEmployeeData] = useState([]);
+function AddDetails() {
+   const recivedData=useContext(ContextData)
     const [employeeDetails, setEmployeeDetails] = useState({
-        fullName: '',
-        designation: '',
-        salary: '',
-        age: ''
+        fullName:'',
+        designation:'',
+        salary:'',
+        age:''
     });
 
     const [nameErrorMessage, setNameErrorMessage] = useState('');
@@ -19,142 +19,132 @@ function AddDetails(props) {
     const [salaryError, setSalaryError] = useState(false);
     const [ageError, setAgeError] = useState(false);
 
-    const handleSubmit = (event) => {
+    const handleSubmit=(event)=>{
         event.preventDefault()
-        const isNameValid = validateName(employeeDetails.fullName)
-        const isDesignationValid = validateDesignation(employeeDetails.designation)
-        const isSalaryValid = validateSalary(employeeDetails.salary)
-        const isAgeValid = validateAge(employeeDetails.age)
+        const isNameValid=validateName(employeeDetails.fullName)
+        const isDesignationValid=validateDesignation(employeeDetails.designation)
+        const isSalaryValid=validateSalary(employeeDetails.salary)
+        const isAgeValid=validateAge(employeeDetails.age)
 
-        if (isNameValid && isDesignationValid && isSalaryValid && isAgeValid) {
-            setEmployeeData([
-                ...employeeData,
-                employeeDetails
-            ])
-            console.log(employeeData);
+        if (isNameValid && isDesignationValid && isSalaryValid && isAgeValid){
+          const array= [...recivedData.storeData]
+          array.push(employeeDetails)
+          recivedData.setStoreData(array)
+            console.log(recivedData.storeData);
             setEmployeeDetails({
-                fullName: '',
-                designation: '',
-                salary: '',
-                age: ''
+                fullName:'',
+                designation:'',
+                salary:'',
+                age:''
             })
             alert('Details Added Successfully')
-        } else {
+        }else {
             console.error('invalid data');
         }
     }
-    //===========================================================================
-    const validateName = (name) => {
-        const nameFormat = /[a-zA-Z]+/
-        if (name === '') {
+//===========================================================================
+    const validateName=(name)=>{
+        const nameFormat= /[a-zA-Z]+/
+        if (name===''){
             setnameError(true)
             setNameErrorMessage('Please enter username')
             return false
-        } else if (nameFormat.test(name)) {
+        }else if (nameFormat.test(name)){
             setnameError(false)
             setNameErrorMessage(' ')
             return true
-        } else {
+        } else{
             setnameError(true)
             setNameErrorMessage('Please enter valid username')
             return false
         }
     }
 
-    const validateDesignation = (designation) => {
-        if (designation) {
+    const validateDesignation=(designation)=>{
+        if (designation){
             setdesignationError(false)
             return true
-        } else {
+        }else{
             setdesignationError(true)
             return false
         }
     }
 
-    const validateSalary = (salary) => {
-        if (salary === '') {
+    const validateSalary=(salary)=>{
+        if (salary===''){
             setSalaryError(true)
             setSalaryErrorMessage('please enter salary')
             return false
         }
-        if (salary > 0) {
+        if (salary>0){
             setSalaryError(false)
             setSalaryErrorMessage(' ')
             return true
-        } else {
+        }else{
             setSalaryError(true)
             setSalaryErrorMessage('salary not valid')
             return false
         }
     }
 
-    const validateAge = (age) => {
-        if (age === '') {
+    const validateAge=(age)=>{
+        if (age===''){
             setAgeError(true)
             setAgeErrorMessage('Please Enter Age')
             return false
-        } else if (age > 18) {
+        }else if (age>18){
             setAgeError(false)
             setAgeErrorMessage('')
             return true
-        } else {
+        }else{
             setAgeError(true)
             setAgeErrorMessage('Age not valid')
             return false
         }
     }
-    //=================================================================================
-    const handleData = (event) => {
+//=================================================================================
+    const handleData=(event)=>{
         setEmployeeDetails({
             ...employeeDetails,
-            [event.target.name]: event.target.value
-
+            [event.target.name]:event.target.value
         })
-
     }
 
-    return <div>
-        <div className='container'></div>
-        <div className='row'>
-            <div className='col-lg-4 m-auto'>
-                <form class="row g-2 form" onSubmit={(event) => { handleSubmit(event) }}>
-                    <div class="form-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Add Employee Details</h5>
-                    </div>
-                    <div class="form-div">
-                        <label class="form-label"><b>Full Name</b></label>
-                        <input type="text" class="form-control" placeholder='Enter full name' onChange={(e) => { handleData(e) }} value={employeeDetails.fullName} name='fullName' />
-                        {nameError ? <div class="error-message text-danger">{nameErrorMessage}</div> : null}
-                    </div>
-
-                    <div class="">
-                        <label class="form-label"><b>Designation</b></label>
-                        <input type="text" class="form-control" placeholder='Enter designation' onChange={(e) => { handleData(e) }} value={employeeDetails.designation} name='designation' />
-                        {designationError ? <span class="error-message text-danger">Please choose a designation.</span> : null}
-
-                    </div>
-
-                    <div class="">
-                        <label class="form-label"><b>Salary</b></label>
-                        <input type="number" class="form-control" placeholder='salary per year' onChange={(e) => { handleData(e) }} value={employeeDetails.salary} name='salary' />
-                        {salaryError ? <span class="error-message text-danger">{salaryErrorMessage}</span> : null}
-                    </div>
-
-                    <div class="">
-                        <label class="form-label"><b>Age</b></label>
-                        <input type="number" class="form-control" placeholder='Enter age' onChange={(e) => { handleData(e) }} value={employeeDetails.age} name='age' />
-                        {ageError ? <span class="error-message text-danger">{ageErrorMessage}</span> : null}
-                    </div>
-
-                    <div class="button">
-                        <button class="btn btn-primary" type="submit" >Submit</button>
-                    </div>
-                </form>
+  return <div class='division'>
+        <form class="row g-2 form shadow rounded " style={{width:'500px', marginLeft:'450px', marginTop:'20px'}}onSubmit={(event)=>{handleSubmit(event)}}>
+            <div class="form-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add Employee Details</h5>
             </div>
-            <div className='col-lg-4'></div>
+            <div class="form-div">
+                <label class="form-label">Full Name</label>
+                <input type="text" class="form-control" placeholder='Enter full name' onChange={(e)=>{handleData(e)}} value={employeeDetails.fullName} name='fullName'/>
+                {nameError ? <div class="error-message">{nameErrorMessage}</div> : null}
+            </div>
 
-        </div>
-    </div>;
+            <div class="">
+                <label class="form-label">Designation</label>
+                <input type="text" class="form-control" placeholder='Enter designation' onChange={(e)=>{handleData(e)}} value={employeeDetails.designation} name='designation'/>
+                {designationError ? <span class="error-message">Please choose a designation.</span> : null }
+                
+            </div>
+
+            <div class="">
+                <label class="form-label">Salary</label>
+                <input type="number" class="form-control" placeholder='salary per year' onChange={(e)=>{handleData(e)}} value={employeeDetails.salary} name='salary'/>
+                {salaryError ? <span class="error-message">{salaryErrorMessage}</span> : null }
+            </div>
+
+            <div class="">
+                <label class="form-label">Age</label>
+                <input type="number" class="form-control" placeholder='Enter age' onChange={(e)=>{handleData(e)}} value={employeeDetails.age} name='age'/>
+                {ageError ? <span class="error-message">{ageErrorMessage}</span> : null }
+            </div>
+
+            <div class="button">
+                <button  class="btn btn-primary " type="submit">Submit</button>
+            </div>
+        </form>
+  </div>;
 }
 
-export default AddDetails;
+export default AddDetails

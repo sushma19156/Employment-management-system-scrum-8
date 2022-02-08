ce22748ac5f0c418c7c3cde6aa034d72ce7668a2
Sushma <sushmaa.m191@gmail.com>
2022-02-08 09:11:39 +0530

    changes in edit

--- src/comp/EditEmployee.js
+++ src/comp/EditEmployee.js
@@ -1,7 +1,6 @@
 import React, { useState,useEffect } from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
-
 function Edit(props) {
     const [show, setShow] = useState(false);
     const [employeeData, setEmployeeData] = useState([]);
